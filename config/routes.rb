78d8fb440beb48271ea531c068e1b31b2dfde97a
tomo.iwamoto <akirouakirouakirou@yahoo.co.jp>
2019-09-04 15:11:09 +0000

    log in

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:create, :new, :show]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
